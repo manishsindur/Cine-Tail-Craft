908821f1c50ce5c15a20c37534577b24a46ef18a
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2024-12-07 10:33:04 +0530

    Added Margin

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -3,11 +3,11 @@ import Footer from "../components/Footer";
 
 function Movies() {
 	return (
-		<>
+		<div className="mx-10 my-5">
 			<Header />
 			<div>Movies</div>
 			<Footer />
-		</>
+		</div>
 	);
 }
 
