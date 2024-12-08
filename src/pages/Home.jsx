fac145189ecfeb91b8c28748e3ba6e39ee848c26
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2024-12-09 00:00:15 +0530

    Imported Streaming and Recent Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import "../components/Header";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Trending from "../layouts/trending-now/Trending";
+import Streaming from "../layouts/streaming/Streaming";
+import Recent from "../layouts/recent-updates/Recent";
 function Home() {
 	return (
 		<div className="px-10 py-5">
