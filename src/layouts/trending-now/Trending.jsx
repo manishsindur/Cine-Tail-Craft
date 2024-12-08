47fff41921e489d3d5627805e9344e9323b1e6dc
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2024-12-08 23:59:00 +0530

    Created Trending Layout

--- src/layouts/trending-now/Trending.jsx
+++ src/layouts/trending-now/Trending.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import "../../styles/streaming/SideBoarder.css";
 
 const Trending = () => {
 	return (
 		<div className="my-10">
 			<nav>
-				<ul className="flex gap-6">
+				<ul className="flex gap-7 w-max py-2 pl-7 pr-3 rounded-xl relative bg-Primary_Black sideWhiteBoarder">
 					<li>
 						<NavLink>Trending Now</NavLink>
 					</li>
