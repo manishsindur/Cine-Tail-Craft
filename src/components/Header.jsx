caf373f69a25ef76d0630effb13a62f08c2d07f5
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2024-12-10 10:42:37 +0530

    Removed logic for welcome page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,65 +1,61 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-	const location = useLocation();
-	const isWelcomePage = location.pathname === "/";
 	return (
 		<header className="flex items-center justify-between">
 			<div className="font-mono text-md">Cine Tail Craft</div>
-			{!isWelcomePage && (
-				<nav>
-					<ul className="flex gap-5">
-						<li>
-							<NavLink
-								to="/home"
-								className={({ isActive, isPending }) =>
-									`${isPending ? "opacity-50" : ""}${
-										isActive ? "text-rose-300" : ""
-									}`
-								}
-							>
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/anime"
-								className={({ isActive, isPending }) =>
-									`${isPending ? "opacity-50" : ""}${
-										isActive ? "text-rose-300" : ""
-									}`
-								}
-							>
-								Anime
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/movies"
-								className={({ isActive, isPending }) =>
-									`${isPending ? "opacity-50" : ""}${
-										isActive ? "text-rose-300" : ""
-									}`
-								}
-							>
-								Movies
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/tvSeries"
-								className={({ isActive, isPending }) =>
-									`${isPending ? "opacity-50" : ""}${
-										isActive ? "text-rose-300" : ""
-									}`
-								}
-							>
-								TV Series
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
-			)}
+			<nav>
+				<ul className="flex gap-5">
+					<li>
+						<NavLink
+							to="/"
+							className={({ isActive, isPending }) =>
+								`${isPending ? "opacity-50" : ""}${
+									isActive ? "text-rose-300" : ""
+								}`
+							}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/anime"
+							className={({ isActive, isPending }) =>
+								`${isPending ? "opacity-50" : ""}${
+									isActive ? "text-rose-300" : ""
+								}`
+							}
+						>
+							Anime
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/movies"
+							className={({ isActive, isPending }) =>
+								`${isPending ? "opacity-50" : ""}${
+									isActive ? "text-rose-300" : ""
+								}`
+							}
+						>
+							Movies
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/tvSeries"
+							className={({ isActive, isPending }) =>
+								`${isPending ? "opacity-50" : ""}${
+									isActive ? "text-rose-300" : ""
+								}`
+							}
+						>
+							TV Series
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
